9bd144b05d5d48a22622c1fe8e84420094c76d1b
LiubomyrFadieiev <helplessmirek@gmail.com>
2021-10-18 15:37:44 +0300

    Add project files.

---
+++ SoftWareProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftWareProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
